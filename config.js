5353d26efbc0ca2ba6e5346d533fb8152cbc537c
patobaird <patobaird@gmail.com>
2021-02-15 06:26:49 -0600

    Create config.js

---
+++ config.js
@@ -0,0 +1,77 @@
+            alignment: 'right',
+            hidden: false,
+            title: 'Community Solutions',
+            image: 'https://cdn.glitch.com/4731465c-ac1a-4510-bae3-525ed0374425%2Fbackinbvi.jpg?v=1612629923249',
+            description: 'As the program grew so did Waves For Water’s solutions. After addressing the immediate need for clean water, Waves For Water focus on their second phase of disaster relief; addressing the breakdowns in water infrastructure through the creation of large and centrally-located water depots. These are big systems that can serve entire communities, rather than just one household. With new implementations happening daily in Puerto Rico and the Outer Islands Waves for Water implemented water-depot systems, across 116 communities and on 8 islands.',
+            location: {
+              center: [-64.70848, 18.33840],
+              zoom: 10.13,
+              pitch: 48.50,
+              bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'dominica-chri',
+            alignment: 'right',
+            hidden: false,
+            title: 'The Journey to Dominica',
+            image: 'https://cdn.glitch.com/4731465c-ac1a-4510-bae3-525ed0374425%2Fdominica.jpg?v=1612629939038',
+            description: 'With a strict curfew on St Croix and very limited transportation from basecamp to the surrounding islands, Waves For Water created a mobile base of operations via sailboat allowing them to be a completely self-sustained unit: transportation, food, shelter, buckets, filters, tanks, and supplies. With 250 nautical miles in front of them, the team set off for the first island in Hurricane Maria’s path, Dominica. The destruction was catastrophic but once on the island Waves for Water was able to reach some of the hardest-hit areas, including the Kalinago Territory.',
+            location: {
+              center: [-61.33167, 15.40194],
+              zoom: 10.39,
+              pitch: 48.50,
+              bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'sustainablefood-chri',
+            alignment: 'right',
+            hidden: false,
+            title: 'Supporting Sustainable Food Solutions',
+            image: 'https://cdn.glitch.com/4731465c-ac1a-4510-bae3-525ed0374425%2Fsustainable.jpg?v=1612629992224',
+            description: 'The scope of the disaster allowed Waves For Water to expand the impact of their programming. In collaboration with local food organizations, W4W built 26 rain catchment systems supporting sustainable farms across the island. These systems not only address the water issues but also provide a substantial means of economic support as these individuals try to recover from catastrophic loss.',
+            location: {
+              center: [-66.48337, 18.06592],
+              zoom: 8.92,
+              pitch: 48.50,
+              bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+      {
+            id: 'final-chri',
+            alignment: 'right',
+            hidden: false,
+            title: 'Total Impact',
+            image: 'https://cdn.glitch.com/4731465c-ac1a-4510-bae3-525ed0374425%2FScreen%20Shot%202021-02-11%20at%207.24.47%20PM.png?v=1613093108852',
+            description: '',
+            location: {
+            center: [-65.86583, 18.29271],
+            zoom: 8.57,
+            pitch: 51.00,
+            bearing: 0.00
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+            ]
+};
+​
